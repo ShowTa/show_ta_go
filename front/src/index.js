bbb837317022cebfdf8171f8102db39a6306fe51
t-kusakabe <ttmac0704@gmail.com>
2018-01-07 04:36:19 +0900

    Add routeing browserrouter

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 import './style.min.css'
 
@@ -8,15 +9,28 @@ import Footer from './layout/footer'
 
 import Top from './top/top'
 
-const App = () => {
+const Routing = () => {
   return(
     <div>
-      <Header />
-      <div className='contents'>
-        <Top />
-      </div>
-      <Footer />
+      <Route exact path='/' component={Top} />
+      <Route path='/article' component={Top} />
+      <Route path='/user' component={Top} />
     </div>
+  )
+}
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className='contents'>
+          <Top />
+        </div>
+        <Footer />
+        <Routing />
+      </div>
+    </BrowserRouter>
   )
 }
 
