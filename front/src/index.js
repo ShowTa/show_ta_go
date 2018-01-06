e2a036a7e6a3f358b7e44ad9a455b179b91b167c
t-kusakabe <ttmac0704@gmail.com>
2018-01-07 03:23:18 +0900

    Load top component in top

--- front/src/index.js
+++ front/src/index.js
@@ -6,11 +6,14 @@ import './style.min.css'
 import Header from './layout/header'
 import Footer from './layout/footer'
 
+import Top from './top/top'
+
 const App = () => {
   return(
     <div>
       <Header />
       <div className='contents'>
+        <Top />
       </div>
       <Footer />
     </div>
