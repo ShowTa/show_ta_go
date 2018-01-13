d0cdfaa59d1f3dd8a72d781c814aff3a63b8e8df
t-kusakabe <ttmac0704@gmail.com>
2018-01-13 23:06:00 +0900

    Create mypage

--- front/src/user/myPage.js
+++ front/src/user/myPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import Article from '../top/article'
+
 export default class MyPage extends React.Component {
   render() {
     return(
-      <div>Welcome MyPage</div>
+      <div className='user'>
+        <div className='user__info'>
+          <div className='user__info__left'>
+            <img src='http://www.nekomasamune.com/download/nekomasamune-g.jpg' className='user__info__left__img' />
+          </div>
+          <div className='user__info__right'>
+            <div className='user__info__right__name'>しょうたさん</div>
+            <div className='user__info__right__favorite'>Total いいね数 : 100いいね</div>
+          </div>
+        </div>
+
+        <div>
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
     )
   }
 }
